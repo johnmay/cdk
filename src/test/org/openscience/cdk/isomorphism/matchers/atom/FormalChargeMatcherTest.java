9204ba9851a861cd58c0e4f3abe7d4d191ae6b6a
John May <john.wilkinsonmay@gmail.com>
2012-07-13 16:37:22 +0100

    Added unit tests for IAtomMatcher implementations
    FormalCharge and Symbol are the current matchers

--- src/test/org/openscience/cdk/isomorphism/matchers/atom/FormalChargeMatcherTest.java
+++ src/test/org/openscience/cdk/isomorphism/matchers/atom/FormalChargeMatcherTest.java
@@ -1,17 +1,72 @@
 package org.openscience.cdk.isomorphism.matchers.atom;
 
 import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IChemObject;
+import org.openscience.cdk.interfaces.IChemObjectBuilder;
+import org.openscience.cdk.silent.SilentChemObjectBuilder;
 
 /**
  * @author John May
  */
 public class FormalChargeMatcherTest {
 
-    private static final Logger LOGGER = Logger.getLogger(FormalChargeMatcherTest.class);
+    private final IChemObjectBuilder builder = SilentChemObjectBuilder.getInstance();
+    private final FormalChargeMatcher matcher = new FormalChargeMatcher();
 
     @Test
-    public void testMatches() throws Exception {
+    public void testMatches_null() throws Exception {
+
+        IAtom query   = builder.newInstance(IAtom.class);
+        IAtom subject = builder.newInstance(IAtom.class);
+
+        query.setFormalCharge(null);
+        subject.setFormalCharge(null);
+
+        Assert.assertTrue(matcher.matches(query, subject));
 
     }
+
+    @Test
+    public void testMatches_value() throws Exception {
+
+        IAtom query   = builder.newInstance(IAtom.class);
+        IAtom subject = builder.newInstance(IAtom.class);
+
+        query.setFormalCharge(0);
+        query.setFormalCharge(0);
+
+        Assert.assertTrue(matcher.matches(query, subject));
+
+    }
+
+    @Test
+    public void testMatches_diffValue() throws Exception {
+
+        IAtom query   = builder.newInstance(IAtom.class);
+        IAtom subject = builder.newInstance(IAtom.class);
+
+        query.setFormalCharge(0);
+        query.setFormalCharge(1);
+
+        Assert.assertFalse(matcher.matches(query, subject));
+
+    }
+
+
+    @Test
+    public void testMatches_oneNull() throws Exception {
+
+        IAtom query   = builder.newInstance(IAtom.class);
+        IAtom subject = builder.newInstance(IAtom.class);
+
+        query.setFormalCharge(0);
+        query.setFormalCharge(null);
+
+        Assert.assertFalse(matcher.matches(query, subject));
+
+    }
+
 }
--- src/test/org/openscience/cdk/isomorphism/matchers/atom/SymbolMatcherTest.java
+++ src/test/org/openscience/cdk/isomorphism/matchers/atom/SymbolMatcherTest.java
@@ -1,17 +1,75 @@
 package org.openscience.cdk.isomorphism.matchers.atom;
 
 import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IChemObjectBuilder;
+import org.openscience.cdk.silent.SilentChemObjectBuilder;
 
 /**
  * @author John May
  */
 public class SymbolMatcherTest {
 
-    private static final Logger LOGGER = Logger.getLogger(SymbolMatcherTest.class);
+    private final IChemObjectBuilder builder = SilentChemObjectBuilder.getInstance();
+    private final IAtomMatcher matcher = new SymbolMatcher();
 
     @Test
-    public void testMatches() throws Exception {
+    public void testMatches_null() throws Exception {
+
+        IAtom query   = builder.newInstance(IAtom.class);
+        IAtom subject = builder.newInstance(IAtom.class);
+
+        query.setSymbol(null);
+        subject.setSymbol(null);
+
+        Assert.assertTrue(matcher.matches(query, subject));
 
     }
+
+    @Test
+    public void testMatches_oneNull() throws Exception {
+
+        IAtom query   = builder.newInstance(IAtom.class);
+        IAtom subject = builder.newInstance(IAtom.class);
+
+        query.setSymbol("?");
+        subject.setSymbol(null);
+
+        Assert.assertFalse(matcher.matches(query, subject));
+
+    }
+
+    @Test
+    public void testMatches_value() throws Exception {
+
+        IAtom query   = builder.newInstance(IAtom.class);
+        IAtom subject = builder.newInstance(IAtom.class);
+
+        query.setSymbol("C");
+        subject.setSymbol("C");
+
+        Assert.assertTrue(matcher.matches(query, subject));
+
+    }
+
+    @Test
+    public void testMatches_diffValue() throws Exception {
+
+        IAtom query   = builder.newInstance(IAtom.class);
+        IAtom subject = builder.newInstance(IAtom.class);
+
+        query.setSymbol("C");
+        subject.setSymbol("O");
+
+        Assert.assertFalse(matcher.matches(query, subject));
+
+    }
+
+    @Test
+    public void testToString(){
+        Assert.assertEquals("query.getSymbol() == subject.getSymbol()", matcher.toString());
+    }
+
 }
