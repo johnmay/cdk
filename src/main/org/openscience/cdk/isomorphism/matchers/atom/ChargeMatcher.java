64ca24f4713783290770df292138d7956c638b59
John May <john.wilkinsonmay@gmail.com>
2012-07-16 09:26:36 +0100

    Added three new matchers

---
+++ src/main/org/openscience/cdk/isomorphism/matchers/atom/ChargeMatcher.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2012 John May <jwmay@users.sf.net>
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+package org.openscience.cdk.isomorphism.matchers.atom;
+
+import org.openscience.cdk.interfaces.IAtom;
+
+/**
+ * An implementation of IAtomMatcher for matching the atom charges
+ *
+ * @author John May
+ * @cdk.module standard
+ * @see org.openscience.cdk.isomorphism.UniversalIsomorphismTester
+ */
+public class ChargeMatcher implements IAtomMatcher {
+
+    /**
+     * Matches charges of the query and subject atoms. If both charge values are
+     * equal or null the match will return true.
+     *
+     * @param query   atom from a query molecule (atom container)
+     * @param subject atom for a subject/target molecule (atom container)
+     *
+     * @return the atom charges match or are both null
+     */
+    @Override
+    public boolean matches(IAtom query, IAtom subject) {
+
+        Double queryCharge   = query.getCharge();
+        Double subjectCharge = subject.getCharge();
+
+        return queryCharge != null ? queryCharge.equals(subjectCharge) : subjectCharge == null;
+
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public String toString() {
+        return "query.getCharge() == subject.getCharge()";
+    }
+
+}
---
+++ src/main/org/openscience/cdk/isomorphism/matchers/atom/MassNumberMatcher.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2012 John May <jwmay@users.sf.net>
+ *
+ * Contact: cdk-devel@lists.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+package org.openscience.cdk.isomorphism.matchers.atom;
+
+import org.openscience.cdk.interfaces.IAtom;
+
+/**
+ * An implementation of IAtomMatcher for matching the atom mass numbers
+ * and can be used to ensure the atoms are the same isotope (i.e. C12 and C13
+ * would not match).
+ *
+ * @author John May
+ * @cdk.module standard
+ * @see org.openscience.cdk.isomorphism.UniversalIsomorphismTester
+ */
+public class MassNumberMatcher implements IAtomMatcher {
+
+    /**
+     * Matches atomic mass numbers of the query and subject atoms. If both mass numbers are
+     * equal or null the match will return true.
+     *
+     * @param query   atom from a query molecule (atom container)
+     * @param subject atom for a subject/target molecule (atom container)
+     *
+     * @return the atom mass numbers match or are both null
+     */
+    @Override
+    public boolean matches(IAtom query, IAtom subject) {
+
+        Integer queryMass   = query.getMassNumber();
+        Integer subjectMass = subject.getMassNumber();
+
+        return queryMass != null ? queryMass.equals(subjectMass) : subjectMass == null;
+
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public String toString() {
+        return "query.getMassNumber() == subject.getMassNumber()";
+    }
+
+}
